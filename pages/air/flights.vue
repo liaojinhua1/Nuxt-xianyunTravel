f37afa074d9b2c4b1e1c45ab56705120c3f0482e
liaojinhua1 <Jin9Code@163.com>
2020-01-02 20:37:57 +0800

    element-ui分页功能完成

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -12,8 +12,22 @@
         <!-- 航班信息 -->
         <!-- :data="item" 把每一项的航班列表信息传递给子组件 -->
         <FlightsItem v-for="(item,index) in dataList" :key="index" :data="item" />
+        <!-- 分页组件 -->
+        <!-- size-change: 切换每页条数时候触发事件
+            current-change: 切换当前页数时候触发的事件
+            current-page: 当然显示的页面
+        total: 总条数-->
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="pageIndex"
+          :page-sizes="[5, 10, 15, 20]"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="flightsData.total"
+        ></el-pagination>
+        <!-- :total="total"> -->
       </div>
-
       <!-- 侧边栏 -->
       <div class="aside">
         <!-- 侧边栏组件 -->
@@ -49,8 +63,27 @@ export default {
         info: {},
         options: {}
       },
-      dataList: [] // 航班列表数据，用于循环flightsItem组件，单独出来是因为要分页
+        // 缓存的变量，当该变量一旦被赋值之后不会被修改
+            // cacheFlightsData: {
+            //     info: {},
+            //     options: {}
+            // },
+      dataList: [], //  // 存放切割出来数据,航班列表数据，用于循环flightsItem组件，单独出来是因为要分页
+      pageSize: 5, // 每页显示条目个数,设置默认值为5
+      pageIndex: 1 //当前页数，设置默认为1
     };
+  },
+  computed: {
+    // // 计算属性监听函数内部引用实例的属性变化，一旦发生了变化，该函数会重新计算，并且返回新的值
+    // dataList() {
+    //   // 请求如果还没完成，返回空数组
+    //   if (!this.flightsData.flights) return [];
+    //   // 计算分页的数据
+    //   return this.flightsData.flights.slice(
+    //     (this.pageIndex - 1) * this.pageSize,
+    //     this.pageIndex * this.pageSize
+    //   );
+    // }
   },
   methods: {
     // 获取航班总数据
@@ -63,8 +96,34 @@ export default {
         this.flightsData = res.data;
         console.log(this.flightsData);
         //  航班列表数据
-        this.dataList = this.flightsData.flights;
+        // this.dataList = this.flightsData.flights;
+        this.setDataList();
       });
+    },
+    // 给过滤的组件修改this.flightsData.flights
+    // setDataList(arr) {
+    //   // arr 就是过滤后的符合条件的数据
+    //   this.flightsData.flights = arr;
+    //   // 修改总条数
+    //   this.total = arr.length;
+    // },
+     // 计算属性监听函数内部引用实例的属性变化，一旦发生了变化，该函数会重新计算，并且返回新的值
+    setDataList() {
+      // 计算分页的数据
+       this.dataList = this.flightsData.flights.slice(
+        (this.pageIndex - 1) * this.pageSize,
+        this.pageIndex * this.pageSize
+      );
+    },
+    // 切换每页条数时候触发事件
+    handleSizeChange(val) {
+      this.pageSize = val;
+      this.setDataList();
+    },
+    // 切换当前页数时候触发的事件
+    handleCurrentChange(val) {
+      this.pageIndex = val;
+      this.setDataList();
     }
   }
 };
@@ -83,5 +142,8 @@ export default {
 
 .aside {
   width: 240px;
+}
+.el-pagination{
+  text-align: center
 }
 </style>
