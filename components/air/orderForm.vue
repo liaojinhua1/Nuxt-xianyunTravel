3b8c99c0149f8584cc95eb5f2d7b93b41136196d
liaojinhua1 <Jin9Code@163.com>
2020-01-03 18:06:35 +0800

    机票订单页请求的机票信息，传递给订单表单组件‘
    git commit -m 机票订单页请求的机票信息，传递给订单表单组件‘

--- components/air/orderForm.vue
+++ components/air/orderForm.vue
@@ -3,9 +3,9 @@
     <div class="air-column">
       <h2>剩机人</h2>
       <el-form class="member-info">
-        <div class="member-info-item" >
-          <el-form-item label="乘机人类型" >
-            <el-input placeholder="姓名" class="input-with-select" v-model="users.username">
+        <div class="member-info-item" v-for="(item,index) in users" :key="index">
+          <el-form-item label="乘机人类型">
+            <el-input placeholder="姓名" class="input-with-select" v-model="item.username">
               <el-select slot="prepend" value="1" placeholder="请选择">
                 <el-option label="成人" value="1"></el-option>
               </el-select>
@@ -13,14 +13,14 @@
           </el-form-item>
 
           <el-form-item label="证件类型">
-            <el-input placeholder="证件号码" class="input-with-select" v-model="users.id">
+            <el-input placeholder="证件号码" class="input-with-select" v-model="item.id">
               <el-select slot="prepend" value="1" placeholder="请选择">
                 <el-option label="身份证" value="1" :checked="true"></el-option>
               </el-select>
             </el-input>
           </el-form-item>
 
-          <span class="delete-user" @click="handleDeleteUser()">-</span>
+          <span class="delete-user" @click="handleDeleteUser(index)">-</span>
         </div>
       </el-form>
 
@@ -64,6 +64,14 @@
 
 <script>
 export default {
+  props: {
+    data: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  },
   data() {
     return {
       users: [
@@ -84,7 +92,7 @@ export default {
   methods: {
     // 添加乘机人
     handleAddUsers() {
-        console.log(111);
+      console.log(111);
       this.users.push({
         username: "",
         id: ""
@@ -92,7 +100,9 @@ export default {
     },
 
     // 移除乘机人
-    handleDeleteUser() {},
+    handleDeleteUser(index) {
+      this.users.splice(index, 1);
+    },
 
     // 发送手机验证码
     handleSendCaptcha() {},
--- pages/air/order.vue
+++ pages/air/order.vue
@@ -3,7 +3,7 @@
     <el-row type="flex" justify="space-between">
       <!-- 订单表单 -->
       <div class="main">
-          <OrderForm/>
+        <OrderForm :data="infoData"/>
       </div>
 
       <!-- 侧边栏 -->
@@ -16,8 +16,30 @@
 // 导入订单表单组件
 import OrderForm from "@/components/air/orderForm.vue";
 export default {
+  data() {
+    return {
+      // 机票信息
+      infoData: {
+        insurances: [] // 初始化保险数据
+      }
+    };
+  },
   components: {
     OrderForm
+  },
+  mounted() {
+    // console.log(this.$route.query);
+    const { id, seat_xid } = this.$route.query;
+    //   发送订单详情页
+    this.$axios({
+      url: "/airs/" + id,
+      parmas: {
+        seat_xid
+      }
+    }).then(res => {
+        this.infoData = res.data
+    //   console.log(res.data);
+    });
   }
 };
 </script>
